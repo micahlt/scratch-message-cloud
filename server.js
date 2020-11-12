11f3431e87ae2b453ebcaa3f4dcbf7d58572ad49
Micah Lindley <micahincorporated@gmail.com>
2020-11-12 08:39:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var s2n = require('string2num');
 var waitUntil = require('wait-until');
 const fs = require('fs');
 var username;
+var testName;
 waitUntil()
   .interval(300)
   .times(Infinity)
@@ -15,9 +16,25 @@ waitUntil()
     Scratch.UserSession.create(process.env.SCRATCH_USERNAME, process.env.SCRATCH_PASSWORD, function(err, user) {
         user.cloudSession(process.env.MESSAGECOUNTID, function(err, cloud) {
             cloud.on('set', function(name, value) {
+              if (name == '☁ Username' && value != undefined) {
+                waitUntil()
+                  .interval(10)
+                  .times(50)
+                  .condition(function () {
+                    return value != undefined;
+                  });
+                username = s2n.decode(value);
+                console.log(username);
+              }
                 if (name == '☁ Update') {
-                    console.log('Update requested...');
-                    username = s2n.decode(cloud.get('☁ Username'));
+                  testName = cloud.get('☁ Username');
+                  waitUntil()
+                  .interval(10)
+                  .times(50)
+                  .condition(function () {
+                    return testName != undefined;
+                  });
+                    console.log('Update requested by ' + username + '(' + testName + ')');
                     cloud.set('☁ Update', 202);
                     fetch('https://api.scratch.mit.edu/users/' + username + '/messages/count').then((res) => {
                         return res.json();
@@ -31,20 +48,6 @@ waitUntil()
                     })
                 }
             })
-            setInterval(function() {
-                console.log('Automatic update triggered...');
-                cloud.set('☁ Update', 202);
-                username = s2n.decode(cloud.get('☁ Username'));
-                fetch('https://api.scratch.mit.edu/users/' + username + '/messages/count').then((res) => {
-                    return res.json();
-                }).then((data) => {
-                    console.log('API request succeeded...\nUpdating messages to ' + data.count + '...');
-                    cloud.set('☁ Messages', data.count);
-                    cloud.set('☁ Update', 200);
-                }).catch((err) => {
-                    cloud.set('☁ Update', 500)
-                })
-            }, 180000)
         })
     })
 });
